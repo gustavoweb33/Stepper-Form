eeba9c4b77b7f537ebe505a619855f840fcd95ca
gustavoweb33 <lealgustavo33@gmail.com>
2020-02-24 10:10:24 -0600

    added func to hide step 1 when in step 2

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 "use strict";
-const buttonOne = document.querySelector('.step-one-button');
-buttonOne === null || buttonOne === void 0 ? void 0 : buttonOne.addEventListener('click', (e) => {
-    const userEmail = document.querySelector('#user-email');
-    console.log(userEmail.value);
+const buttonOne = document.querySelector('.js-step-one');
+const buttonTwo = document.querySelector('.js-step-two');
+const fetchUserEmail = document.querySelector('#js-email');
+const stepTwo = document.querySelector('.step-two');
+stepTwo.style.visibility = 'hidden';
+fetchUserEmail.addEventListener('input', (event) => {
+    let userEmail = event.target.value;
+    if (userEmail.length !== 0) {
+        buttonOne.disabled = false;
+    }
+    else {
+        buttonOne.disabled = true;
+    }
+});
+buttonOne === null || buttonOne === void 0 ? void 0 : buttonOne.addEventListener('click', () => {
+    if (stepTwo.style.visibility === 'hidden') {
+        stepTwo.style.visibility = 'visible';
+    }
+    else {
+        stepTwo.style.visibility = 'hidden';
+    }
+    const stepOne = document.querySelector('.step-one-form');
+    stepOne.style.visibility = 'hidden';
+});
+buttonTwo === null || buttonTwo === void 0 ? void 0 : buttonTwo.addEventListener('click', () => {
+    const fetchUserName = document.querySelector('#js-name');
+    const userName = fetchUserName.value;
+    console.log(userName);
 });
--- index.ts
+++ index.ts
@@ -1,7 +1,36 @@
-const buttonOne = document.querySelector('.step-one-button');
+const buttonOne = document.querySelector('.js-step-one') as HTMLButtonElement;
+const buttonTwo = document.querySelector('.js-step-two') as HTMLButtonElement
+const fetchUserEmail = document.querySelector('#js-email')! as HTMLInputElement;
+const stepTwo = document.querySelector('.step-two') as HTMLElement;
+stepTwo.style.visibility = 'hidden';
 
-buttonOne?.addEventListener('click', (e) => {
-    const userEmail= document.querySelector('#user-email')! as HTMLInputElement;
-    console.log(userEmail.value)
+fetchUserEmail.addEventListener('input', (event) => {
+    let userEmail = (event.target as HTMLInputElement).value;
+
+    if (userEmail.length !== 0) {
+        buttonOne.disabled = false;
+    }
+    else {
+        buttonOne.disabled = true;
+    }
+
 });
 
+buttonOne?.addEventListener('click', () => {
+    if (stepTwo.style.visibility === 'hidden') {
+        stepTwo.style.visibility = 'visible';
+    }
+    else {
+        stepTwo.style.visibility = 'hidden';
+    }
+
+    const stepOne = document.querySelector('.step-one-form') as HTMLElement;
+    stepOne.style.visibility = 'hidden';
+});
+
+buttonTwo?.addEventListener('click', () => {
+
+    const fetchUserName = document.querySelector('#js-name')! as HTMLInputElement;
+    const userName = fetchUserName.value;
+    console.log(userName)
+});
